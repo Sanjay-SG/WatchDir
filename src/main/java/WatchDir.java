19abb283d2216f1bf004046d82f26f23eb3fe4ba
Sanjay S G <sanjay.s.gunagi@gmail.com>
2019-09-10 17:22:51 +0530

    Initial Commit.

---
+++ src/main/java/WatchDir.java
@@ -0,0 +1,40 @@
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+public class WatchDir {
+
+	public static void main(String[] args) throws IOException,
+			InterruptedException {
+
+		Path faxFolder = Paths.get("C:\\Sanjay");//("./fax/");
+		WatchService watchService = FileSystems.getDefault().newWatchService();
+		faxFolder.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+
+		boolean valid = true;
+		do {
+			WatchKey watchKey = watchService.take();
+
+			for (WatchEvent event : watchKey.pollEvents()) {
+				WatchEvent.Kind kind = event.kind();
+				if (StandardWatchEventKinds.ENTRY_CREATE.equals(event.kind())) {
+					String fileName = event.context().toString();
+					System.out.println("File Created:" + fileName);
+					if(fileName.contains("FLTS"))
+					{
+						System.out.println("File arrived.");
+					}
+				}
+			}
+			valid = watchKey.reset();
+
+		} while (valid);
+
+	}
+}
